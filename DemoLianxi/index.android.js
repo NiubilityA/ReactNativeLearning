ec03daf29a46077f4f527b2d9d105608df54613c
Coding24h <zhangzhengyu@coding24h.com>
2017-05-15 16:31:02 +0800

    闪烁文字

---
+++ DemoLianxi/index.android.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class DemoLianxi extends Component {
+  render() {
+  	let pic = {
+     	 uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+	   <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
+class Blink extends Component{
+	constructor(props){
+		super(props);
+		this.state = {showText: true};
+		
+		//每1000毫秒对showText状态做一次取反操作
+		setInterval(() => {
+			this.setState(previousState => {
+				return {showText: !previousState.showText};
+			});
+		},1000);
+	}
+	
+	render() {
+		let display = this.state.showText ? this.props.text : ' ';
+		return (
+			<Text>{display}</Text>
+		);
+	}
+}
+class Greeting extends Component {
+	render(){
+		return (
+			<Text>Hello {this.props.na}!</Text>
+		)
+	}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('DemoLianxi', () => DemoLianxi);
